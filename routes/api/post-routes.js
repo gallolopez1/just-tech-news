ed558e1a960cb48c08774887b44e6606d9a252fe
Your Name <gallolopez1@hotmail.com>
2021-12-21 14:53:50 -0600

    Removed some comments

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -77,7 +77,7 @@ router.get('/:id', (req, res) => {
 });
 
 router.post('/', (req, res) => {
-  // expects {title: 'Taskmaster goes public!', post_url: 'https://taskmaster.com/press', user_id: 1}
+
   Post.create({
     title: req.body.title,
     post_url: req.body.post_url,
@@ -91,7 +91,7 @@ router.post('/', (req, res) => {
 });
 
 router.put('/upvote', (req, res) => {
-  // custom static method created in models/Post.js
+
   Post.upvote(req.body, { Vote, Comment, User })
     .then(updatedVoteData => res.json(updatedVoteData))
     .catch(err => {
